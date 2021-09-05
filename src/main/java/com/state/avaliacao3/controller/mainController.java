7574424bf9648e4c4a00b6e27eeaccfb924aa702
itallocunha <80712552+itallocunha@users.noreply.github.com>
2021-09-05 20:38:52 -0300

    Swagger states
    Completo

--- src/main/java/com/state/avaliacao3/controller/mainController.java
+++ src/main/java/com/state/avaliacao3/controller/mainController.java
@@ -3,6 +3,8 @@ package com.state.avaliacao3.controller;
 import com.state.avaliacao3.model.State;
 import com.state.avaliacao3.service.DBService;
 import com.state.avaliacao3.service.StateService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/states")
+@Api(value= "API REST states")
+@CrossOrigin(origins="*")
 public class mainController {
 
     @Autowired
@@ -23,6 +27,7 @@ public class mainController {
     DBService dbService;
 
     @PostMapping("/")
+    @ApiOperation(value="Salva um states")
     public ResponseEntity <State> postNewCar(@RequestBody State stateForm) throws ParseException {
         boolean right = stateService.itsValid(stateForm);
 
@@ -35,6 +40,7 @@ public class mainController {
     }
 
     @PutMapping("/{id}")
+    @ApiOperation(value="Atualiza um state")
     public ResponseEntity <State> putStateById(@PathVariable Long id, @RequestBody State formState) throws ParseException{
 
         boolean right = stateService.itsValid(formState);
@@ -48,6 +54,7 @@ public class mainController {
     }
 
     @GetMapping("/")
+    @ApiOperation(value="Returna uma lista de states")
     public ResponseEntity<List<State>> listAllStates() throws ParseException {
        dbService.instanciaBaseDados();
         List<State> listStates = stateService.findAllStates();
@@ -55,30 +62,35 @@ public class mainController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value="Retorna um unico state")
     public ResponseEntity <State> listStateById(@PathVariable Long id) throws ParseException {
         State state = stateService.findStateById(id);
         return ResponseEntity.ok().body(state);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity <List<State>> deleteAllCars(@PathVariable Long id) throws ParseException {
+    @ApiOperation(value="Deleta uma lista de states")
+    public ResponseEntity <List<State>> deleteAllState(@PathVariable Long id) throws ParseException {
         stateService.deleteStateById(id);
         List<State> listState = stateService.findAllStates();
         return ResponseEntity.ok().body(listState);
     }
 
     @GetMapping("/region/{region}")
+    @ApiOperation(value="Retorna regiao")
     public ResponseEntity <List<State>> listAllStateByRegion(@PathVariable String region) throws ParseException {
         List<State> listState = stateService.findAllStatesByRegion(region);
         return ResponseEntity.ok().body(listState);
     }
 
     @GetMapping("/population/{population}")
+    @ApiOperation(value="Retorna populacao")
     public ResponseEntity <List<State>> listAllStateByPopulation(@PathVariable BigDecimal population) throws ParseException {
         List<State> listState = stateService.findAllCarsByPopulation(population);
         return ResponseEntity.ok().body(listState);
     }
     @GetMapping("/color/{color}")
+    @ApiOperation(value="Retorna cor")
     public ResponseEntity <List<State>> listAllStateByArea(@PathVariable BigDecimal area) throws ParseException {
         List<State> listArea = stateService.findAllStateByArea(area);
         return ResponseEntity.ok().body(listArea);
